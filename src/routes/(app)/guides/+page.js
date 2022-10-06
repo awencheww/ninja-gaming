472af78c63676debb4161125cd41c0b4843cc2e9
awencheww <cherwenm@gmail.com>
2022-10-06 14:05:20 +0800

    Fix cross-origin issue

--- src/routes/(app)/guides/+page.js
+++ src/routes/(app)/guides/+page.js
@@ -2,7 +2,7 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
   const url = 'https://jsonplaceholder.typicode.com/posts'
-  const config = { method: 'get', headers: { origin: 'http://localhost:5173'} }
+  const config = { method: 'get', headers: { origin: '*'} }
   const request = new Request(url, config)
   const res = await fetch(request)
   const guides = await res.json()
