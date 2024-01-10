c7d55cbdfaaf3ec5558b082edf0aa896df917696
Cherwen Molina <cherwenm@gmail.com>
2024-01-10 21:34:19 +0800

    Update +page.svelte

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="index">
-  <h2>Welcome</h2>
+  <h2>Welcome Awencheww!</h2>
   <p>Fugiat ex sunt excepteur laboris dolore reprehenderit officia. Lorem cupidatat velit magna incididunt cupidatat cillum irure dolore quis dolore aliquip. Velit amet labore esse ad sunt reprehenderit et sint proident commodo laborum occaecat. Velit laborum nisi Lorem occaecat sint. Fugiat dolore anim commodo minim quis nulla magna tempor do ea. Ea Lorem est cupidatat laboris. Est cillum nulla incididunt minim laboris duis velit.</p>
 
   <a href="/guides">View Guides</a>
